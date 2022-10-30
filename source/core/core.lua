735cc908fbabd7b96491dd6da470dd68d26a1095
Ling_2121 <2609344951@qq.com>
2022-10-30 23:30:35 +0800

    更改初始化方式

--- source/core/core.lua
+++ source/core/core.lua
@@ -3,18 +3,17 @@
 love.graphics.setDefaultFilter("nearest","nearest")
 
 core = {
-    Thread;                 --多线程模块
-    Resources;              --资源管理模块
-    EntityCountructor;      --实体构造器
-    ComponentConstructor;   --组件构造器
-    Utilities;              --实用工具集
+    Containers = require"source/core/base/container/containers";                    --基本容器
+    Utilities = require"source/core/base/utilities";                                --实用工具集
+    EntityCountructor = require"source/core/constructor/entity_constructor";        --实体构造器
+    ComponentConstructor = require"source/core/constructor/component_constructor";  --组件构造器
+
+    Thread;                                                                         --多线程模块
+    Resources;                                                                      --资源管理模块
 }
 
 function core:load()
     self.Thread = require"source/core/thread/thread"()
-    self.EntityCountructor = require"source/core/constructor/entity_constructor"
-    self.ComponentConstructor = require"source/core/constructor/component_constructor"
-    self.Utilities = require"source/core/base/utilities"
     self.Resources = require"source/core/resources/resources"("")
 end
 
